84aa0f7a33c7e0626d72d47fc6ffc1b287c36ff2
Sergii Vostrikov <ext-sergii.vostrikov@here.com>
2019-08-06 17:11:51 +0300

    TaskScheduler added to OlpClientSettings
    In order to setup OlpClientSettings user should fill-in task_scheduler
    field. One of the ways to do that is so use newly introduced method
    of OlpClientSettingsFactory class.
    
    Resolves: OLPEDGE-453
    
    Signed-off-by: Sergii Vostrikov <ext-sergii.vostrikov@here.com>

---
+++ olp-cpp-sdk-core/include/olp/core/client/OlpClientSettingsFactory.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+#pragma once
+
+#include <olp/core/CoreApi.h>
+
+#include <memory>
+
+namespace olp {
+namespace thread {
+class TaskScheduler;
+}
+
+namespace client {
+/**
+ * @brief Factory that helps to fill-in OlpClientSettings structure with various
+ * default handlers.
+ */
+class CORE_API OlpClientSettingsFactory final {
+ public:
+  /**
+   * @brief Creates a TaskScheduler instance used for all delayed operations,
+   * which is defaulted to olp::thread::ThreadPoolTaskScheduler with the only 1
+   * thread set up.
+   * @return An instance of TaskScheduler.
+   */
+  static std::unique_ptr<olp::thread::TaskScheduler> DefaultTaskScheduler(
+      size_t thread_count = 1u);
+};
+
+}  // namespace client
+
+}  // namespace olp
