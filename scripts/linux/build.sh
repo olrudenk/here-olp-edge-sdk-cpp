63242aa111f61cb0d45ae4f3340b667b8bd01a58
Oleksandr Rudenko <52194521+olrudenk@users.noreply.github.com>
2019-08-09 15:25:11 +0300

    Update build.sh

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -2,7 +2,7 @@
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -EDGE_SDK_BUILD_EXAMPLES=ON -DEDGE_SDK_BUILD_DOC=ON ..
 make -j8
-cmake --build . --target docs
+make docs
 cd ..
 #CPP_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication
 CPP_TEST_SOURCE_AUTHENTICATION=build/olp-cpp-sdk-authentication/test
